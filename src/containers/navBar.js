00af3014a222a9edc870fa1d386afd92d46b7b80
Lee <delutejohnz@gmail.com>
2022-05-04 12:30:08 +0800

    mao najud ni

--- src/containers/navBar.js
+++ src/containers/navBar.js
@@ -3,7 +3,7 @@ import NavBarMain from '../components/navBar/navBarMain'
 import NavBarMenu from '../components/navBar/navBarMenu'
 function NavBar() {
     return (
-        <nav className = "xs:fixed xs:w-full sm:sticky sm:top-0 md:sticky md:top-0 lg:sticky lg:top-0 flex bg-blue p-7 font-mainFont h-[5rem] z-50">
+        <nav className = "fixed w-full flex bg-blue p-7 font-mainFont h-[5rem] z-50">
             <div className='w-full'>
                 <NavBarMain/>
             </div>
