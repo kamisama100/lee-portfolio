bf75132b97b01491950be94f82149a9f2e033cf2
Lee <delutejohnz@gmail.com>
2022-06-15 23:50:54 +0800

    update

--- src/containers/projects.js
+++ src/containers/projects.js
@@ -42,6 +42,24 @@ function Projects() {
                             Login Form
                         </p>
                     </div>
+                    <div className='space-y-2'>
+                        <div className="w-[350px] h-[220px] rounded-2xl bg-[url('./images/p3.PNG')] bg-cover bg-center">
+                            <div className='text-white place-items-center relative opacity-0 transition ease-in-out hover:opacity-100 duration-300 h-full bg-black bg-opacity-60 backdrop-blur-sm grid place-content-center rounded-2xl space-y-2'>
+                                <h2 className='w-fit h-fit text-[21px] opacity-80 '>
+                                    FCC Certification Project for Responsive Design
+                                </h2>
+                                <p className='w-[80%] text-[18px] '>
+                                    Created using <span className='text-green-500'>HTML</span>, and <span className='text-green-500'>CSS</span>.
+                                </p>
+                                <a href='https://kamisama100.github.io/fcc-proj/' target="_blank" without rel="noreferrer" className='border-b-[1px]' >
+                                    Go to Website
+                                </a>    
+                            </div>
+                        </div>
+                        <p className='text-[19px] opacity-70'>
+                            FCC Certification Project
+                        </p>
+                    </div>
                 </div>
             </div>
         </section>
